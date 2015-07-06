0def133d5c689d2c6076defabadb5610747e505a
SpiritWolf2015 <522414019@qq.com>
2015-07-06 22:52:59 +0800

    1.1
    ViewPagger切换，文字变色效果完成。

--- src/com/gzc/weixin5ui/MainActivity.java
+++ src/com/gzc/weixin5ui/MainActivity.java
@@ -1,14 +1,105 @@
 package com.gzc.weixin5ui;
 
-import android.app.Activity;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.widget.TextView;
 
+public class MainActivity extends FragmentActivity {
 
-public class MainActivity extends Activity {
+	private ViewPager mViewPager = null;
+	private FragmentPagerAdapter mAdapter = null;
+	private List<Fragment> mDatas = null;
+	
+	private TextView mTextView1;
+	private TextView mTextView2;
+	private TextView mTextView3;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		initView();
 	}
+
+	private void initView() {
+		mTextView1 = (TextView)findViewById(R.id.id_tv_chat);
+		mTextView2 = (TextView)findViewById(R.id.id_tv_friend);
+		mTextView3 = (TextView)findViewById(R.id.id_tv_contact);		
+		
+		mViewPager = (ViewPager) findViewById(R.id.id_viewpager);
+		No1Fragment tab1 = new No1Fragment();
+		No2Fragment tab2 = new No2Fragment();
+		No3Fragment tab3 = new No3Fragment();
+
+		mDatas = new ArrayList<Fragment>();
+		mDatas.add(tab1);
+		mDatas.add(tab2);
+		mDatas.add(tab3);
+
+		mAdapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
+
+			@Override
+			public android.support.v4.app.Fragment getItem(int location) {
+				return mDatas.get(location);
+			}
+
+			@Override
+			public int getCount() {
+				return mDatas.size();
+			}
+
+		};
+		// 设置适配器
+		this.mViewPager.setAdapter(mAdapter);
+
+		mViewPager.setOnPageChangeListener(new OnPageChangeListener() {
+			@Override
+			public void onPageSelected(int position) {
+				resetTextView();
+				
+				switch(position){
+				case 0:
+					// 滑动到这个页面，改变字体颜色。
+					mTextView1.setTextColor(Color.parseColor("#EE0000"));
+					break;
+				case 1:
+					// 滑动到这个页面，改变字体颜色。
+					mTextView2.setTextColor(Color.parseColor("#EE0000"));
+					break;
+				case 2:
+					// 滑动到这个页面，改变字体颜色。
+					mTextView3.setTextColor(Color.parseColor("#EE0000"));
+					break;
+				}
+			}
+
+			@Override
+			public void onPageScrollStateChanged(int arg0) {
+				// TODO 自动生成的方法存根
+
+			}
+
+			@Override
+			public void onPageScrolled(int arg0, float arg1, int arg2) {
+				// TODO 自动生成的方法存根
+
+			}
+		});
+	}
+	
+	private void resetTextView(){
+		mTextView1.setTextColor(Color.BLACK);
+		mTextView2.setTextColor(Color.BLACK);
+		mTextView3.setTextColor(Color.BLACK);
+	}
+
 }
---
+++ src/com/gzc/weixin5ui/No1Fragment.java
@@ -0,0 +1,24 @@
+package com.gzc.weixin5ui;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * 使用V4包的Fragment可以向下兼容SDK版本，我这就直接用V4的。 注意一定要统一使用，要么都V4包， 要么都是VIEW包的，不要混着用，不然会报错。
+ * 
+ * @author gzc
+ *
+ */
+public class No1Fragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.tab1, container, false);
+	}
+
+}
