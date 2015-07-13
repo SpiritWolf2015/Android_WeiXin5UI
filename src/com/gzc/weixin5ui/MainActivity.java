b0b117b8a1be9047cf830e0acf3cbdcc9c4bb2fb
SpiritWolf2015 <522414019@qq.com>
2015-07-13 22:06:56 +0800

    1.2
    指示器效果完成。

--- src/com/gzc/weixin5ui/MainActivity.java
+++ src/com/gzc/weixin5ui/MainActivity.java
@@ -10,6 +10,12 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Display;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 import android.widget.TextView;
 
 public class MainActivity extends FragmentActivity {
@@ -17,24 +23,47 @@ public class MainActivity extends FragmentActivity {
 	private ViewPager mViewPager = null;
 	private FragmentPagerAdapter mAdapter = null;
 	private List<Fragment> mDatas = null;
-	
+
 	private TextView mTextView1;
 	private TextView mTextView2;
 	private TextView mTextView3;
+
+	final String CHANGE_COLOR = "#EE0000";
+
+	private ImageView mTabLine = null;
+	/** 屏幕三分之1宽度 */
+	private int mScreen1_3;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		initTabLine();
 		initView();
 	}
 
+	private void initTabLine() {
+		mTabLine = (ImageView) this.findViewById(R.id.id_iv_tabline);
+
+		Display display = this.getWindowManager().getDefaultDisplay();
+		DisplayMetrics outMetrics = new DisplayMetrics();
+		display.getMetrics(outMetrics);
+		// 得到屏幕宽度的三分之1
+		mScreen1_3 = outMetrics.widthPixels / 3;
+
+		android.view.ViewGroup.LayoutParams lp = mTabLine.getLayoutParams();
+		lp.width = mScreen1_3;
+		Log.e("指示线", "宽度 = " + mScreen1_3);
+		// 相当于在代码里设置XML界面参数
+		mTabLine.setLayoutParams(lp);
+	}
+
 	private void initView() {
-		mTextView1 = (TextView)findViewById(R.id.id_tv_chat);
-		mTextView2 = (TextView)findViewById(R.id.id_tv_friend);
-		mTextView3 = (TextView)findViewById(R.id.id_tv_contact);		
-		
+		mTextView1 = (TextView) findViewById(R.id.id_tv_chat);
+		mTextView2 = (TextView) findViewById(R.id.id_tv_friend);
+		mTextView3 = (TextView) findViewById(R.id.id_tv_contact);
+
 		mViewPager = (ViewPager) findViewById(R.id.id_viewpager);
 		No1Fragment tab1 = new No1Fragment();
 		No2Fragment tab2 = new No2Fragment();
@@ -48,7 +77,7 @@ public class MainActivity extends FragmentActivity {
 		mAdapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
 
 			@Override
-			public android.support.v4.app.Fragment getItem(int location) {
+			public Fragment getItem(int location) {
 				return mDatas.get(location);
 			}
 
@@ -65,19 +94,19 @@ public class MainActivity extends FragmentActivity {
 			@Override
 			public void onPageSelected(int position) {
 				resetTextView();
-				
-				switch(position){
+
+				switch (position) {
 				case 0:
 					// 滑动到这个页面，改变字体颜色。
-					mTextView1.setTextColor(Color.parseColor("#EE0000"));
+					mTextView1.setTextColor(Color.parseColor(CHANGE_COLOR));
 					break;
 				case 1:
 					// 滑动到这个页面，改变字体颜色。
-					mTextView2.setTextColor(Color.parseColor("#EE0000"));
+					mTextView2.setTextColor(Color.parseColor(CHANGE_COLOR));
 					break;
 				case 2:
 					// 滑动到这个页面，改变字体颜色。
-					mTextView3.setTextColor(Color.parseColor("#EE0000"));
+					mTextView3.setTextColor(Color.parseColor(CHANGE_COLOR));
 					break;
 				}
 			}
@@ -89,14 +118,21 @@ public class MainActivity extends FragmentActivity {
 			}
 
 			@Override
-			public void onPageScrolled(int arg0, float arg1, int arg2) {
-				// TODO 自动生成的方法存根
+			public void onPageScrolled(int position, float positionOffset,
+					int positionOffsetPx) {
 
+				LinearLayout.LayoutParams lp = (LayoutParams) mTabLine
+						.getLayoutParams();
+//				if (positionOffset > 0) {
+					lp.leftMargin = (int) (positionOffset * mScreen1_3 + position
+							* mScreen1_3);
+//				}
+				mTabLine.setLayoutParams(lp);
 			}
 		});
 	}
-	
-	private void resetTextView(){
+
+	private void resetTextView() {
 		mTextView1.setTextColor(Color.BLACK);
 		mTextView2.setTextColor(Color.BLACK);
 		mTextView3.setTextColor(Color.BLACK);
